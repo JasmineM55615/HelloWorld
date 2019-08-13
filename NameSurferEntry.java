f363164f02af8cf33949c584fe3a9d40f3454f50
CS106A <>
2019-08-12 21:30:17 -0700

    {"types":"C","runs":10}

--- NameSurferEntry.java
+++ NameSurferEntry.java
@@ -75,7 +75,7 @@ public class NameSurferEntry implements NameSurferConstants {
 		// You need to turn this stub into a real implementation //
 		String str = name + " [ ";
 		for(int i = 0; i < NDECADES; i ++) {
-			String num = numbers[i];
+			str += numbers[i] + ", ";
 		}
 		return "";
 	}
