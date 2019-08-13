4ed0bfdf3e98624409343bd72e2e957afa05afaf
CS106A <>
2019-08-13 07:35:12 -0700

    {"types":"C","runs":31}

--- NameSurfer.java
+++ NameSurfer.java
@@ -74,9 +74,8 @@ public class NameSurfer extends GraphicsProgram implements NameSurferConstants {
 						add(nameLabel, xPointOne, getHeight() - GRAPH_MARGIN_SIZE);
 						yPointOne = getHeight()-GRAPH_MARGIN_SIZE;
 					}else {
-						GLabel nameLabel = new GLabel("" + theName + " " + numOfName);
+						GLabel nameLabel = new GLabel("" + theName + " " + ((int) numOfName);
 						yPointOne = (numOfName / 1000) * (getHeight() - (GRAPH_MARGIN_SIZE * 2)) + GRAPH_MARGIN_SIZE;
-						System.out.println(yPointOne);
 						add(nameLabel, xPointOne, yPointOne);
 					}
 					if(mathYTwo == 0 || mathYTwo == 1000) {
