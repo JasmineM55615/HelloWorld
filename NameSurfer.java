0b94756f2dc8c19806669f8a90ec8c69b3017e14
CS106A <>
2019-08-13 06:13:05 -0700

    {"types":"C","runs":28}

--- NameSurfer.java
+++ NameSurfer.java
@@ -49,7 +49,7 @@ public class NameSurfer extends GraphicsProgram implements NameSurferConstants {
 		}
 		char c = Character.toUpperCase(newStrName.charAt(0));
 		String newNew = c + newStr;
-		if(e.getActionCommand().equalsIgnoreCase("Graph")){
+		if(e.getActionCommand().equals("Graph")){
 			redraw();
 			println("You pressed enter or the button");
 			println("Graph: " + newNew);
