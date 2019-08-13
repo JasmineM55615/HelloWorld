ca04cc41d2e6771ba7fd5f118b465ffb5835bcd6
CS106A <>
2019-08-13 01:11:40 -0700

    {"types":"C","runs":22}

--- NameSurfer.java
+++ NameSurfer.java
@@ -99,7 +99,7 @@ public class NameSurfer extends GraphicsProgram implements NameSurferConstants {
 			for(int j = 0; j < NDECADES; j++) {
 				int YOne = inputs.get(i).getRank(j);
 				if(YOne == 0 || YOne == 1000) {
-					String nameLabel = 
+					String nameLabel = ""
 				}
 			}
 		}
