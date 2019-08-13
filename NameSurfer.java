34821e101d6e6ead602786960c9c4f5380865afe
CS106A <>
2019-08-13 06:18:31 -0700

    {"types":"C","runs":31}

--- NameSurfer.java
+++ NameSurfer.java
@@ -81,9 +81,10 @@ public class NameSurfer extends GraphicsProgram implements NameSurferConstants {
 			GLine line = new GLine(XSpace, 0, XSpace, getHeight());
 			XSpace += getWidth() / NDECADES;
 			add(line);
-			System.out.println("hi");
+			//System.out.println("hi");
 		}
 		GLine otherLineOne = new GLine(0, GRAPH_MARGIN_SIZE, getWidth(), GRAPH_MARGIN_SIZE);
+		System.out.println("hi");
 		add(otherLineOne);
 		GLine otherLineTwo = new GLine(0, getHeight() - GRAPH_MARGIN_SIZE, getWidth(), getHeight() - GRAPH_MARGIN_SIZE);
 		add(otherLineTwo);
